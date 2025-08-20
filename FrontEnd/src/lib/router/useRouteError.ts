7b344bda6ac46a487a1b7c6619ebbd54be3deb96
Prashant Chauhan <prashant.chauhan@coachbar.io>
2025-08-20 14:20:51 +0530

    chore(frontend): removed frontend code

--- FrontEnd/src/lib/router/useRouteError.ts
+++
@@ -1,7 +0,0 @@
-import { useRouteError as useReactRouterError } from "react-router";
-
-import type { AjaxError } from "@/lib/http/AjaxError";
-
-export const useRouteError = () => {
-  return useReactRouterError() as AjaxError;
-};
